e32b08a821d03ab55fc0a9745ae9aca038831603
lanht <2318203932@qq.com>
2023-09-17 23:11:31 +0800

    move files

---
+++ lib/base_study/flutter/first_app/state/widget_state_mix.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Center(child: ParentWidget())
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+   @override
+  _ParentWidgetState createState() =>  _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  const TapboxC({Key key, this.active: false, this.onChanged}):super(key: key);
+
+  @override
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _hightlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _hightlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _hightlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _hightlight = false;
+    });
+  }
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(
+             widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(
+              fontSize: 32.0,
+              color: Colors.white
+            ),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _hightlight ? Border.all(
+            color: Colors.teal[700],
+            width: 10.0
+          ) : null,
+        ),
+      ),
+    );    
+  }
+}
--- lib/base_study/flutter/first_app/state/widget_state_super.dart
+++ lib/base_study/flutter/first_app/state/widget_state_super.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ParentWidget()
+      home: Center(child: ParentWidget())
     );
   }
 }
